a6900a1bd8ee61e47de234cec2be43b9b4cd0087
Felipe Mamede <felipebrasilmv@gmail.com>
2023-05-20 16:56:56 -0300

    :art: Aula 04

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,8 @@
 // REACT
 import { useEffect } from 'react'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 // EXPO
-import { StatusBar } from 'expo-status-bar'
 import { useRouter } from 'expo-router'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import * as SecureStore from 'expo-secure-store'
@@ -11,23 +10,8 @@ import * as SecureStore from 'expo-secure-store'
 // API
 import { api } from '@lib/api'
 
-// STYLE
-import { styled } from 'nativewind'
-
-// FONTS
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
 // ASSETS
-import blurBg from '@assets/bg-blur.png'
-import Stripes from '@assets/stripes.svg'
 import NLWLogo from '@assets/nlw-spacetime-logo.svg'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -38,12 +22,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -83,18 +61,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -122,8 +90,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
